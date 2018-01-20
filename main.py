56430fb2d1bbddfe6ee8696a5995bf45c45a88a4
ltopuser <ltopuser@LTOP-LIBHD-001.ed.ac.uk>
2018-01-20 15:14:00 +0000

    render video

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 app.debug = True
@@ -6,7 +6,7 @@ app.debug = True
 
 @app.route('/')
 def home():
-    return "YES"
+    return render_template('video.html')
 
 if __name__ == "__main__":
     app.run()
