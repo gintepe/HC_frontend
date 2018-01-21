d18d5a16a21d2b01a147d5c0d427e24972700220
ginte <ginte.p@gmail.com>
2018-01-21 11:28:25 +0000

    rolling average

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ from flask import Flask, render_template
 import shapiro
 import tak
 import cameron
-from statistics import mean
 app = Flask(__name__)
 app.debug = True
 
@@ -15,12 +14,12 @@ def home():
 @app.route('/tak')
 def take():
     hr = tak.hr_data[0]['hr']
-    return render_template('video.html', people=tak.hr_data, yid=tak.hr_data[0]['video'], max=round(max(hr)), min=round(min(hr)), average=round(mean(hr)) )
+    return render_template('video.html', people=tak.hr_data, yid=tak.hr_data[0]['video'], max=round(max(hr)), min=round(min(hr)), average=0 )
 
 @app.route('/cam')
 def cam():
     hr = cameron.hr_data[0]['hr']
-    return render_template('video.html', people=cameron.hr_data, yid=cameron.hr_data[0]['video'] + "", max=round(max(hr)), min=round(min(hr)), average=round(mean(hr)) )
+    return render_template('video.html', people=cameron.hr_data, yid=cameron.hr_data[0]['video'] + "", max=round(max(hr)), min=round(min(hr)), average=0)
 
 if __name__ == "__main__":
     app.run()
