5501ff395951a1893d96688288b635de0e987be4
ltopuser <ltopuser@LTOP-LIBHD-001.ed.ac.uk>
2018-01-21 11:03:42 +0000

    aaa

--- main.py
+++ main.py
@@ -1,14 +1,26 @@
 from flask import Flask, render_template
-from temp_data import hr_data
+import shapiro
+import tak
+import cameron
+from statistics import mean
 app = Flask(__name__)
 app.debug = True
 
 
+
 @app.route('/')
 def home():
-    for i, person in enumerate(hr_data):
-        hr_data[i]['img_url'] = "https://imgur.com/IFYVIH4.png"
-    return render_template('video.html', people=hr_data)
+
+    return render_template('video.html', people=shapiro.hr_data, yid=shapiro.hr_data[0]['video'])
+@app.route('/tak')
+def take():
+    hr = tak.hr_data[0]['hr']
+    return render_template('video.html', people=tak.hr_data, yid=tak.hr_data[0]['video'], max=round(max(hr)), min=round(min(hr)), average=round(mean(hr)) )
+
+@app.route('/cam')
+def cam():
+    hr = cameron.hr_data[0]['hr']
+    return render_template('video.html', people=cameron.hr_data, yid=cameron.hr_data[0]['video'], max=round(max(hr)), min=round(min(hr)), average=round(mean(hr)) )
 
 if __name__ == "__main__":
     app.run()
--- static/js/bbox_test.js
+++ static/js/bbox_test.js
@@ -3,14 +3,17 @@ function bbox() {
 	console.log(hr_data[0]['bbox']);
 	for (i=0; i< hr_data[0]['bbox'].length; i++) {
 		setTimeout(function(i){
-			bbox_pos = hr_data[0]['bbox'][i]
-			console.log(bbox_pos);
+			s = 1.3;
+			var h = $('.video').height()*s;
+			var w = $('.video').width()*s;
+			console.log(h, w)
+			bbox_pos = hr_data[0]['bbox'][i];
 			bbox = document.getElementsByClassName("bbox")[0]
-			bbox.style.left = bbox_pos.left*100+'%';
-			bbox.style.top = bbox_pos.top*100+'%';
+			bbox.style.left = bbox_pos.left*w+'px';
+			bbox.style.top = bbox_pos.top*h+'px';
 			// bbox.style.bottom = bbox_pos.bottom+'px';
-			bbox.style.width = (bbox_pos.right-bbox_pos.left)*100+'%';
-			bbox.style.height =(bbox_pos.bottom-bbox_pos.top)*100+'%';
+			bbox.style.width = (bbox_pos.right-bbox_pos.left)*w+'px';
+			bbox.style.height =(bbox_pos.bottom-bbox_pos.top)*h+'px' ;
 			
 		},1000/fps*i,i)
 	}
