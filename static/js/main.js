823eb478705fc7d17e2c568fd9cbcbaf099a9d0f
ltopuser <ltopuser@LTOP-LIBHD-001.ed.ac.uk>
2018-01-21 08:17:46 +0000

    ojojoi

--- static/js/main.js
+++ static/js/main.js
@@ -3,25 +3,33 @@
     function resize_graph() {
         $('.graph').attr('width', crow.width());
         $('.graph').attr('height', $('.col-name-person').height());
-
-        // $('.row-stats').height($('.row-video').outerHeight(true) - $('.row-graph').outerHeight(true) - $('.row-person-info').outerHeight(true))
-    }
+        }
     $(window).resize(resize_graph);
     resize_graph();
 
     var series_settings = {lineWidth:2,strokeStyle:'#fb3e28'};
     var chart_settings = {grid:{strokeStyle:'#f5f5f5', fillStyle:'#f5f5f5', borderVisible:false}, labels:{disabled:true} };
 
-    var chart = new SmoothieChart(chart_settings),
-        canvas = document.getElementsByClassName('graph')[0];
+
+    $.each(hr_data, function (i, person) {
+        var j = (i + 1).toString();
+        var index = 0;
+        var chart = new SmoothieChart(chart_settings),
+        canvas = document.getElementsByClassName('g' + j)[0],
         series = new TimeSeries();
+        chart.addTimeSeries(series, series_settings);
+        chart.streamTo(canvas, 1000);
 
-    chart.addTimeSeries(series, series_settings);
-    chart.streamTo(canvas, 1000);
+        setInterval(function () {
 
-    setInterval(function () {
-       series.append(new Date().getTime(), Math.random())
-    },1000);
+            series.append(new Date().getTime(), person['hr'][index]);
+            index += 1;
+        }, 1000)
+    });
+
+
+
+
 
 // });
 
