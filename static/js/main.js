9bf9ba6fd7f572d532dfa773b47717978185d6cf
ginte <ginte.p@gmail.com>
2018-01-21 11:17:01 +0000

    mutable max min

--- static/js/main.js
+++ static/js/main.js
@@ -1,4 +1,3 @@
-// $(function () {
     var crow = $('.row-graph');
     function resize_graph() {
         $('.graph').attr('width', crow.width());
@@ -14,6 +13,10 @@
     $.each(hr_data, function (i, person) {
         var j = (i + 1).toString();
         var index = 0;
+        var min = document.getElementById('min');
+        var max = document.getElementById('max');
+        max.innerText = Math.round(person['hr'][index]);
+        min.innerText = Math.round(person['hr'][index]);
         var chart = new SmoothieChart(chart_settings),
         canvas = document.getElementsByClassName('g' + j)[0],
         series = new TimeSeries();
@@ -23,6 +26,11 @@
         setInterval(function () {
             series.append(new Date().getTime(), person['hr'][index]);
             $('.p' + j + ' .hr-live').text(Math.round(person['hr'][index]));
+            if ( person['hr'][index] < min.innerText ) {
+                min.innerText = Math.round(person['hr'][index]);
+            } else if ( person['hr'][index] > max.innerText ) {
+                max.innerText = Math.round(person['hr'][index]);
+            }
             index += 1;
         }, 1000)
     });
@@ -30,6 +38,4 @@
 
 
 
-
-// });
 
